691da8b3505090b47202de3fcbd560f7ca49c06e
Oscar Torres <166583974+O-Torress@users.noreply.github.com>
2026-08-03 23:31:59 -0400

    Ventana lista e imagenes extraidas

---
+++ src/app.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class app {
+  
+  public static void main(String[]arg) throws Exception {
+    int rowCount = 21;
+    int columnCount = 19;
+    int tileSize = 32;
+    int boardWidth = columnCount * tileSize;
+    int boardHeight = rowCount * tileSize;
+
+
+    JFrame frame = new JFrame("Pac Man");
+    frame.setVisible(true);
+    frame.setSize(boardHeight, boardWidth);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
